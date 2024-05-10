5211819195d1710b97b35393f86dcdbf8399aef4
moursalinme <me.moursalin@gmail.com>
2024-05-10 12:17:59 +0600

    cloud server

--- rw/server.js
+++ rw/server.js
@@ -12,7 +12,7 @@ mongoose
 const port = process.env.port || 3000;
 console.log(process.env.NODE_ENV);
 // if(process.env.NODE_ENV === 'development') {
-  app.listen(port, ()=> console.log(`Server running on port ${port}`));
+  app.listen(port, ()=> console.log(`Server running on port http://localhost:${port}`));
 // } else {
   // console.log('Can not run the production UNIT.');
 // }
